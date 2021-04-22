aaf08b8a1e07864922f4d2e48c22f87b160c579c
Alexey <slag88@mail.ru>
2021-04-22 16:23:13 +0300

    2 repository error

--- src/main/java/web/repository/UserRepository.java
+++ src/main/java/web/repository/UserRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import web.model.User;
 
 import java.util.Optional;
-
+//******************************************
 public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findByName(String name);      //НЕ АКТИВЕН!!!
 }
